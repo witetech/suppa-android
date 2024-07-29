959032c3ae2bbe82ffd5d918205f767e95e20bd5
Ömer Karaca <omerkaracca@gmail.com>
2024-07-29 21:02:44 +0300

    Add BitmapUtils

---
+++ suppa/src/main/kotlin/ai/suppa/presentation/utils/BitmapUtils.kt
@@ -0,0 +1,16 @@
+package ai.suppa.presentation.utils
+
+import android.graphics.*
+import android.util.Base64
+
+object BitmapUtils {
+    private fun convertBase64ImageToBitmap(imageBase64: String): Bitmap? {
+        val substring = imageBase64.substringAfter("base64,")
+        val decodedString: ByteArray = Base64.decode(substring, Base64.DEFAULT)
+        return try {
+            BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
+        } catch (e: Exception) {
+            null
+        }
+    }
+}
